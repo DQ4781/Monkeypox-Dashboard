eee37d0e20f67b23044804253675707dcfccb682
danqueLINUX <dquezada478@gmail.com>
2022-11-09 16:23:32 -0800

    initial GIS page

---
+++ pages/2_🌐_GIS.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pydeck as pdk
+from data import *
+
+
+st.set_page_config(
+    page_title="GIS",
+    page_icon="🌐",
+    initial_sidebar_state="collapsed"
+)
+
+st.title("GIS Data")
+
+
