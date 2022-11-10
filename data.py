e3001a0f42d5b916615c5c8a130f7a345f95e800
danqueLINUX <dquezada478@gmail.com>
2022-11-09 16:23:01 -0800

    revereted back to data.py

---
+++ data.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+
+
+state_total = pd.read_csv("csv/data-table.csv")
+nation_cum  = pd.read_csv("csv/7dayAvg.csv")
+state_vac   = pd.read_csv("csv/vaccinesAdmin.csv")
+gender_tests = pd.read_csv("csv/nvo-data.csv")
+gender_confirm = pd.read_csv("csv/ageGender.csv")
+race_percent = pd.read_csv("csv/raceWeeklyPercent.csv")
+symptoms = pd.read_csv("csv/symptoms.csv")
+
+
+state_total.drop([0,53,40], inplace=True)
+state_vac.drop([0,1,2,57], inplace=True)
+
+nation_cum['epi_date_V2'] = pd.to_datetime(nation_cum['epi_date_V2'])
+race_percent['MMWR Week'] = pd.to_datetime(race_percent['MMWR Week'])
+
