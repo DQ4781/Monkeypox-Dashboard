a2e415f6bfaf9a2c8098dc5d9fa0aef00c91b1c4
danqueLINUX <dquezada478@gmail.com>
2022-11-25 07:22:52 -0800

    refactor weeklyDifference module,

--- pages/1_👥_Demographics.py
+++ pages/1_👥_Demographics.py
@@ -37,48 +37,63 @@ def drawPieChart():
 def rateOfChange(val1, val2):
     return int((val1/val2 -1) * 100)
 
-# assumes that week is the index
+# Assumes that wk1 & wk2 are gender_tests rows
 def calculateRateofChange(wk1, wk2):
-    tmt     = str(rateOfChange(wk1[0], wk2[0])) + "%"
-    mtpr    = str(rateOfChange(wk1[1], wk2[1])) + "%"
-    tft     = str(rateOfChange(wk1[2], wk2[2])) + "%"
-    ftpr    = str(rateOfChange(wk1[3], wk2[3])) + "%"
+    tmt     = str(rateOfChange(wk1[1], wk2[1])) + "%"
+    mtpr    = str(rateOfChange(wk1[2], wk2[2])) + "%"
+    tft     = str(rateOfChange(wk1[3], wk2[3])) + "%"
+    ftpr    = str(rateOfChange(wk1[4], wk2[4])) + "%"
 
     return [tmt, mtpr, tft, ftpr] 
 
-def displayMetrics(labels,cmpr,values):
+# labels=... uses a gender_tests.head()
+# values=... the later week row
+# deltas=... comes from calculateRateOfChange
+def displayMetrics(labels,values,deltas,single):
     col1, col2, col3, col4 = st.columns(4)
 
-    col1.metric(label=labels[0], value=cmpr[0], delta=values[0])
-    col2.metric(label=labels[1], value=cmpr[1], delta=values[1])
-    col3.metric(label=labels[2], value=cmpr[2], delta=values[2])
-    col4.metric(label=labels[3], value=cmpr[3], delta=values[3])  
+    if not single:
+        col1.metric(label=labels[1], value=values[1],           delta=deltas[0])
+        col2.metric(label=labels[2], value=str(values[2])+"%",  delta=deltas[1])
+        col3.metric(label=labels[3], value=values[3],           delta=deltas[2])
+        col4.metric(label=labels[4], value=str(values[4])+"%",  delta=deltas[3])
+    else:
+        col1.metric(label=labels[1], value=values[1]           )
+        col2.metric(label=labels[2], value=str(values[2])+"%"  )
+        col3.metric(label=labels[3], value=values[3]           )
+        col4.metric(label=labels[4], value=str(values[4])+"%"  )
 
 
 def weeklyDifference():
-    gt = gender_tests.copy(deep=True)
-    gt.set_index('Week', inplace=True)
 
     button_label = "Select Two Weeks to Compare"
-    metric_label = list(gt.head())
+    metric_label = list(gender_tests.head())
     weeks = gender_tests['Week']
 
-    dfWk1 = gt.iloc[0]
-    dfWk2 = gt.iloc[-1]
+    # Retrieves the first & last week rows
+    dfWk1 = gender_tests.iloc[0]
+    dfWk2 = gender_tests.iloc[-1]
 
-    week_button = st.multiselect(options=weeks, label=button_label, default=[dfWk1[0], dfWk2[0]])
+    week_button = st.multiselect(options=weeks, label=button_label)
 
-    while True:
-        # User deleted a date; continue displaying the default weeks
-        if len(week_button) != 2: 
-            rOfc = calculateRateofChange(dfWk1, dfWk2)
-            displayMetrics(metric_label, dfWk2, rOfc)
-        # Multiselect has two dates; display the difference
-        elif len(week_button) == 2:
-            wk1 = gt.loc[min(week_button)]
-            wk2 = gt.loc[max(week_button)]
-            rOfc = calculateRateofChange(wk1, wk2)
-            displayMetrics(metric_label, wk2, rOfc)
+    if len(week_button) == 0:
+        deltas = calculateRateofChange(wk1=dfWk1, wk2=dfWk2)
+        displayMetrics(labels=metric_label,values=dfWk2,deltas=deltas,single=False)
+    elif len(week_button) == 1:
+        displayMetrics(labels=metric_label, values=week_button[0], deltas=None, single=True)
+    elif len(week_button) == 2:
+        x = min(week_button)
+        y = max(week_button)
+
+        gender_tests.set_index('Week', inplace=True)
+        minRow = gender_tests.loc[x]
+        maxRow = gender_tests.loc[y]
+        gender_tests.reset_index(inplace=True)
+        
+        deltas = calculateRateofChange(wk1=minRow, wk2=maxRow)
+        displayMetrics(labels=metric_label, values=maxRow, deltas=deltas, single=False)
+    else:
+        st.error("Too many arguments!", icon="🚨")
     
 
 def weeklyRace():
@@ -98,6 +113,10 @@ def main():
     drawPieChart()
     weeklyRace()
     weeklyDifference()
+
+    cumCases()
+    dailyCases()
+    sevenDayAvg()
 
 
 main()
