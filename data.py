7e75d99e8d7e5b6e72e4cfba39847f8092fa1719
danqueLINUX <dquezada478@gmail.com>
2022-12-19 14:31:40 -0800

    updated csv names in data.py

--- data.py
+++ data.py
@@ -2,12 +2,12 @@ import pandas as pd
 
 
 
-state_total = pd.read_csv("csv/data-table.csv")
+state_total = pd.read_csv("csv/stateCase.csv")
 nation_cum  = pd.read_csv("csv/7dayAvg.csv")
-state_vac   = pd.read_csv("csv/vaccinesAdmin.csv")
-gender_tests = pd.read_csv("csv/nvo-data.csv")
-gender_confirm = pd.read_csv("csv/ageGender.csv")
-race_percent = pd.read_csv("csv/raceWeeklyPercent.csv")
+state_vac   = pd.read_csv("csv/vaccines.csv")
+gender_tests = pd.read_csv("csv/labTests.csv")
+gender_confirm = pd.read_csv("csv/genders.csv")
+race_percent = pd.read_csv("csv/ethincity.csv")
 symptoms = pd.read_csv("csv/symptoms.csv")
 
 # Drop US territories; only want Continental US
@@ -26,4 +26,4 @@ state_vac.set_index("Reporting Jurisdictions", inplace=True)
 nyc = state_vac['Total'].loc["New York City"]
 state_vac.at["New York", "Total"] += nyc
 state_vac.drop(labels="New York City", inplace=True)
-state_vac.reset_index(inplace=True)
+state_vac.reset_index(inplace=True
