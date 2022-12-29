4f7cc1337f94515acc1159bb98781b25418c234b
danqueLINUX <dquezada478@gmail.com>
2022-12-28 22:42:21 -0800

    added dt.dates & created new col on gender_tests

--- data.py
+++ data.py
@@ -15,15 +15,19 @@ state_total.drop([0,53,40], inplace=True)
 state_vac.drop([0,1,2,57], inplace=True)
 
 # Reformat calendar dates to ISO 8601 
-nation_cum['epi_date_V2']   = pd.to_datetime(nation_cum['epi_date_V2'], format='%Y-%m-%d', errors='coerce', utc=True)
-race_percent['MMWR Week']   = pd.to_datetime(race_percent['MMWR Week'], format='%Y-%m-%d', errors='coerce', utc=True)
-gender_tests['Week']        = pd.to_datetime(gender_tests['Week'], format='%Y-%m-%d', errors='coerce', utc=True)
+nation_cum['epi_date_V2']   = pd.to_datetime(nation_cum['epi_date_V2']).dt.date
+race_percent['MMWR Week']   = pd.to_datetime(race_percent['MMWR Week']).dt.date
+gender_tests['Week']        = pd.to_datetime(gender_tests['Week']).dt.date
 
-# Reindex Gender Tests
-#gender_tests.set_index('Week', inplace=True)
+# Reindex
+gender_tests.set_index('Week', inplace=True)
 
+# Create new sum columns
+gender_tests = gender_tests.assign(Total_Tests=gender_tests['Total Male Tests']+gender_tests['Total Female Tests'])
+
+# Add NYC to NY's total vaccine count
 state_vac.set_index("Reporting Jurisdictions", inplace=True)
 nyc = state_vac['Total'].loc["New York City"]
 state_vac.at["New York", "Total"] += nyc
 state_vac.drop(labels="New York City", inplace=True)
-state_vac.reset_index(inplace=True
+state_vac.reset_index(inplace=True)
